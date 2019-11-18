f687f9c521faa7f406265aeec321cd4246dcd9f7
Nida Ilyas <38118425+ni-da@users.noreply.github.com>
2019-11-18 02:51:19 +0100

    added node.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,10 +25,3 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
